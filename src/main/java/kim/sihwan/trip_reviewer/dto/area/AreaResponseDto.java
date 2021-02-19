54477d380228c29b43f4d41f8f3215dc60b15081
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 16:17:17 +0900

    지역 dto

--- src/main/java/kim/sihwan/trip_reviewer/dto/area/AreaResponseDto.java
+++ src/main/java/kim/sihwan/trip_reviewer/dto/area/AreaResponseDto.java
@@ -1,4 +1,28 @@
 package kim.sihwan.trip_reviewer.dto.area;
 
+import kim.sihwan.trip_reviewer.domain.Area;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AreaResponseDto {
+    private Long id;
+    private int idx;
+    private String name;
+    private String title;
+    private String color;
+    private String accompany;
+    private String visitDate;
+
+    public AreaResponseDto(Area area){
+        id = area.getId();
+        idx = area.getIdx();
+        name= area.getName();
+        title = area.getTitle();
+        color = area.getColor();
+        accompany = area.getAccompany();
+        visitDate = area.getVisitDate();
+    }
+
 }
