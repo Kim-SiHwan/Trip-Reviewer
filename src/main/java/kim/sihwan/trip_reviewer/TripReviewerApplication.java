8c608b36627a89b795b6f7812311df1e2deaea8a
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 11:58:56 +0900

    new project

---
+++ src/main/java/kim/sihwan/trip_reviewer/TripReviewerApplication.java
@@ -0,0 +1,13 @@
+package kim.sihwan.trip_reviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripReviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripReviewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kim/sihwan/trip_reviewer/TripReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.sihwan.trip_reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripReviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
