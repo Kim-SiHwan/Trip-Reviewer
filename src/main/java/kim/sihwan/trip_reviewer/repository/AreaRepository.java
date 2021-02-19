ea3df37c109fee59cdc5527c7f14603077f42747
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 16:17:07 +0900

    지역 repository 쿼리 작성.

--- src/main/java/kim/sihwan/trip_reviewer/repository/AreaRepository.java
+++ src/main/java/kim/sihwan/trip_reviewer/repository/AreaRepository.java
@@ -3,5 +3,8 @@ package kim.sihwan.trip_reviewer.repository;
 import kim.sihwan.trip_reviewer.domain.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AreaRepository extends JpaRepository<Area,Long> {
+    List<Area> findAllByMember_Username(String username);
 }
