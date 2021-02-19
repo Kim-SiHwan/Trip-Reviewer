5f2197bad64d042d1a4f4e65f52b40affd562260
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 21:36:46 +0900

    댓글 dto

--- src/main/java/kim/sihwan/trip_reviewer/dto/comment/CommentRequestDto.java
+++ src/main/java/kim/sihwan/trip_reviewer/dto/comment/CommentRequestDto.java
@@ -1,4 +1,23 @@
 package kim.sihwan.trip_reviewer.dto.comment;
 
+import kim.sihwan.trip_reviewer.domain.Comment;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class CommentRequestDto {
+    private Long reviewId;
+    private String username;
+    private String content;
+
+    public Comment toEntity(CommentRequestDto requestDto){
+        return Comment.builder()
+                .content(requestDto.getContent())
+                .username(requestDto.getUsername())
+                .createDate(LocalDateTime.now())
+                .build();
+    }
 }
