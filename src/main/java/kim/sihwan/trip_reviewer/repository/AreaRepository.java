29df7295356a1b3a526d9fa3b5a339f9d65ff314
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 13:55:47 +0900

    지역 저장소 생성

---
+++ src/main/java/kim/sihwan/trip_reviewer/repository/AreaRepository.java
@@ -0,0 +1,7 @@
+package kim.sihwan.trip_reviewer.repository;
+
+import kim.sihwan.trip_reviewer.domain.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaRepository extends JpaRepository<Area,Long> {
+}
