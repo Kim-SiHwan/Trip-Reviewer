35af84ae02a417e54896264e9cfe91c2b45733e7
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 16:17:31 +0900

    앨범 dto

--- src/main/java/kim/sihwan/trip_reviewer/dto/area/album/AlbumRequestDto.java
+++ src/main/java/kim/sihwan/trip_reviewer/dto/area/album/AlbumRequestDto.java
@@ -3,21 +3,14 @@ package kim.sihwan.trip_reviewer.dto.area.album;
 import kim.sihwan.trip_reviewer.domain.Album;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
 @Setter
 public class AlbumRequestDto {
     private Long areaId;
-    private String url;
-    private String filename;
-    private String originFilename;
-
-    public Album toEntity(AlbumRequestDto requestDto){
-        return Album.builder()
-                .url(requestDto.getUrl())
-                .filename(requestDto.getFilename())
-                .originFilename(requestDto.getOriginFilename())
-                .build();
-    }
+    private List<MultipartFile> files;
 
 }
