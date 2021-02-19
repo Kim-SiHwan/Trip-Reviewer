80a5aff02a8b28a4ffe325d140006a10d1597a40
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 21:41:56 +0900

    리뷰 서비스 리뷰와 리뷰 앨범의 생성, 조회 , 삭제 기능 구현

--- src/main/java/kim/sihwan/trip_reviewer/service/ReviewService.java
+++ src/main/java/kim/sihwan/trip_reviewer/service/ReviewService.java
@@ -1,9 +1,12 @@
 package kim.sihwan.trip_reviewer.service;
 
 import kim.sihwan.trip_reviewer.domain.*;
+import kim.sihwan.trip_reviewer.dto.review.ReviewListResponseDto;
 import kim.sihwan.trip_reviewer.dto.review.ReviewRequestDto;
 import kim.sihwan.trip_reviewer.dto.review.ReviewResponseDto;
+import kim.sihwan.trip_reviewer.repository.CommentRepository;
 import kim.sihwan.trip_reviewer.repository.MemberRepository;
+import kim.sihwan.trip_reviewer.repository.ReviewAlbumRepository;
 import kim.sihwan.trip_reviewer.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,19 +25,22 @@ import java.util.stream.Collectors;
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final MemberRepository memberRepository;
+    private final CommentRepository commentRepository;
+    private final ReviewAlbumRepository reviewAlbumRepository;
+    private final ReviewAlbumService reviewAlbumService;
 
-    public List<ReviewResponseDto> findAllReviews(){
+    public List<ReviewListResponseDto> findAllReviews(){
         List<Review> reviews = reviewRepository.findAll();
-        List<ReviewResponseDto> list = reviews
+        List<ReviewListResponseDto> list = reviews
                 .stream()
-                .map(review -> new ReviewResponseDto(review))
+                .map(review -> new ReviewListResponseDto(review))
                 .collect(Collectors.toList());
         Collections.reverse(list);
         return list;
     }
 
     public ReviewResponseDto findOneByReviewId(Long reviewId){
-        Review review = reviewRepository.findById(reviewId).get();
+        Review review = reviewRepository.findReviewById(reviewId);
         ReviewResponseDto reviewResponseDto = new ReviewResponseDto(review);
         return reviewResponseDto;
     }
@@ -43,7 +49,7 @@ public class ReviewService {
     public void addReview(ReviewRequestDto requestDto){
         Member member = memberRepository.findMemberByUsername(requestDto.getUsername()).get();
         Review review = requestDto.toEntity(requestDto);
-        String fileUrl = "C:\\Users\\김시환\\Desktop\\Git\\Trip-Reviewer\\src\\main\\resources\\static\\albumImages\\";
+        String fileUrl = "C:\\Users\\김시환\\Desktop\\Git\\Trip-Reviewer\\src\\main\\resources\\static\\reviewImages\\";
         String saveUrl = "http://localhost:8080/api/album/download?filename=";
 
         try{
@@ -68,8 +74,21 @@ public class ReviewService {
 
     @Transactional
     public void deleteReview(Long reviewId){
-        Review review = reviewRepository.findById(reviewId).get();
+        Review review = reviewRepository.findReviewById(reviewId);
+        List<Long> commentIds = review.getComments()
+                .stream()
+                .map(comment -> comment.getId())
+                .collect(Collectors.toList());
+        List<Long> reviewAlbumIds = review.getReviewAlbums()
+                .stream()
+                .map(reviewAlbum -> reviewAlbum.getId())
+                .collect(Collectors.toList());
+        System.out.println(reviewAlbumIds);
+        commentRepository.deleteAllByIdInQuery(commentIds);
+
+        reviewAlbumService.deleteReviewAlbum(reviewAlbumIds);
         reviewRepository.delete(review);
+
     }
 
     public String createNewFilename(String filename){
