f236c3d8126a34aa276a339457172e71973d24d5
Kim-SiHwan <tjdans4166@naver.com>
2021-02-19 16:18:27 +0900

    앨범 repository 데이터 삭제 시 성능 개선을 위해 기본 제공해주는 deleteById 사용하지않고 (이건 삭제 할 데이터의 수 *2만큼 쿼리가 나갔음 ) 쿼리를 직접 작성해 1번의 쿼리로만 리스트를 가지고 삭제할 수 있도록 개선함.

--- src/main/java/kim/sihwan/trip_reviewer/repository/AlbumRepository.java
+++ src/main/java/kim/sihwan/trip_reviewer/repository/AlbumRepository.java
@@ -1,7 +1,22 @@
 package kim.sihwan.trip_reviewer.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import kim.sihwan.trip_reviewer.domain.Album;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface AlbumRepository extends JpaRepository<Album,Long> {
+// 굳이?   @EntityGraph(attributePaths = {"area"}, type = EntityGraph.EntityGraphType.LOAD)
+    List<Album> findAllByArea_Id(Long id);
+
+    //이걸로 delete는 쿼리 한번으로 퉁.
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Album a WHERE a.id IN :ids")
+    void deleteAllAlbumByQuery(@Param("ids") List<Long> ids);
 }
