87a8fb55e0c26f1848cbcf5ee139e93c66a47274
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 17:50:49 +0900

    쿼리 생성

--- src/main/java/kim/sihwan/trip_reviewer/repository/ReviewTagRepository.java
+++ src/main/java/kim/sihwan/trip_reviewer/repository/ReviewTagRepository.java
@@ -1,18 +1,19 @@
 package kim.sihwan.trip_reviewer.repository;
 
 import io.lettuce.core.dynamic.annotation.Param;
-import kim.sihwan.trip_reviewer.domain.Tag;
+import kim.sihwan.trip_reviewer.domain.ReviewTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface TagRepository extends JpaRepository<Tag,Long> {
+public interface ReviewTagRepository extends JpaRepository<ReviewTag,Long> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ReviewTag r WHERE r.id IN :ids")
+    void deleteAllByIdInQuery(@Param("ids") List<Long> ids);
 
-    Optional<Tag> findTagByTag(String tag);
-
-
+    List<ReviewTag> findAllByTag_Id(Long id);
 }
