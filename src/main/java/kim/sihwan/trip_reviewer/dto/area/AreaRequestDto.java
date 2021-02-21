bf9ebebcb7b15e33024f3144d99fe88e6860bad0
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 17:48:05 +0900

    지역구 요청 dto

--- src/main/java/kim/sihwan/trip_reviewer/dto/area/AreaRequestDto.java
+++ src/main/java/kim/sihwan/trip_reviewer/dto/area/AreaRequestDto.java
@@ -1,4 +1,14 @@
 package kim.sihwan.trip_reviewer.dto.area;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AreaRequestDto {
+    private Long areaId;
+    private String title;
+    private String color;
+    private String accompany;
+    private String visitDate;
 }
