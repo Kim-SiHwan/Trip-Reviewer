68d97722d143b152cf7bfc37e3c09865dad93051
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 17:43:57 +0900

    jwt 토큰 필터 설정

---
+++ src/main/java/kim/sihwan/trip_reviewer/config/jwt/JwtFilter.java
@@ -0,0 +1,50 @@
+package kim.sihwan.trip_reviewer.config.jwt;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+public class JwtFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider tokenProvider;
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String jwt = resolveToken(httpServletRequest);
+        String requestURI = httpServletRequest.getRequestURI();
+
+
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            System.out.println("유효한 토큰이 없습니다." + requestURI);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
