cfbc666ef89c213f4aabf2913c2fe677a6ed1a27
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 14:29:52 +0900

    리뷰를 삭제할 때 리뷰에 달린 사진들을 을 한번의 쿼리로 삭제하게 변경함. 기존에는 댓글의 수 만큼 Delete 쿼리가 작성되었으나 1번으로 줄임.

--- src/main/java/kim/sihwan/trip_reviewer/service/ReviewAlbumService.java
+++ src/main/java/kim/sihwan/trip_reviewer/service/ReviewAlbumService.java
@@ -1,11 +1,19 @@
 package kim.sihwan.trip_reviewer.service;
 
+import kim.sihwan.trip_reviewer.domain.Member;
+import kim.sihwan.trip_reviewer.domain.Review;
+import kim.sihwan.trip_reviewer.domain.ReviewAlbum;
+import kim.sihwan.trip_reviewer.dto.review.ReviewRequestDto;
+import kim.sihwan.trip_reviewer.dto.review.reviewAlbum.ReviewAlbumRequestDto;
 import kim.sihwan.trip_reviewer.repository.ReviewAlbumRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,4 +27,35 @@ public class ReviewAlbumService {
         reviewAlbumRepository.deleteAllByIdInQuery(reviewAlbumIdList);
     }
 
+    @Transactional
+    public Review addReviewAlbums(ReviewRequestDto requestDto){
+        Review review = requestDto.toEntity(requestDto);
+        String fileUrl = "C:\\Users\\김시환\\Desktop\\Git\\Trip-Reviewer\\src\\main\\resources\\static\\reviewImages\\";
+        String saveUrl = "http://localhost:8080/api/album/download?filename=";
+
+        try{
+            String newFilename="";
+            for(MultipartFile file : requestDto.getFiles()){
+                newFilename = createNewFilename(file.getOriginalFilename());
+                File dest = new File(fileUrl + newFilename);
+                file.transferTo(dest);
+                ReviewAlbum reviewAlbum = ReviewAlbum
+                        .builder()
+                        .url(saveUrl + newFilename)
+                        .filename(newFilename)
+                        .originFilename(file.getOriginalFilename())
+                        .build();
+                reviewAlbum.addReview(review);
+            }
+            review.addThumbnail(saveUrl + newFilename);
+        }catch (Exception e){
+            System.out.println("리뷰 앨범 생성 중 오류 발생");
+        }
+        return review;
+    }
+    public String createNewFilename(String filename){
+        UUID uuid = UUID.randomUUID();
+        String newFilename= uuid.toString() +"_" + filename;
+        return newFilename;
+    }
 }
