691129bde7a3ccd1435ee9eecdfd1fa7171ef307
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 17:49:04 +0900

    수정, 초기화 기능 추가

--- src/main/java/kim/sihwan/trip_reviewer/controller/AreaController.java
+++ src/main/java/kim/sihwan/trip_reviewer/controller/AreaController.java
@@ -1,13 +1,14 @@
 package kim.sihwan.trip_reviewer.controller;
 
 import kim.sihwan.trip_reviewer.domain.Area;
+import kim.sihwan.trip_reviewer.dto.area.AreaRequestDto;
 import kim.sihwan.trip_reviewer.dto.area.AreaResponseDto;
 import kim.sihwan.trip_reviewer.service.AreaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +21,8 @@ public class AreaController {
     //쿼리1
     @GetMapping
     public List<AreaResponseDto> findAllByUsername(){
-        List<AreaResponseDto> list = areaService.findAllByUsername("kim");
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<AreaResponseDto> list = areaService.findAllByUsername(username);
         return list;
     }
 
@@ -29,5 +31,17 @@ public class AreaController {
     public AreaResponseDto findOneByAreaId(@PathVariable("areaId")Long areaId){
         AreaResponseDto area = areaService.findOneByAreaId(areaId);
         return area;
+    }
+
+    @PutMapping
+    public ResponseEntity updateAreaInfo(@RequestBody AreaRequestDto requestDto){
+        areaService.changeAreaInfo(requestDto);
+        return new ResponseEntity(areaService.findOneByAreaId(requestDto.getAreaId()), HttpStatus.OK);
+    }
+
+    @PutMapping("/{areaId}")
+    public ResponseEntity initArea(@PathVariable("areaId")Long areaId){
+        areaService.initArea(areaId);
+        return new ResponseEntity(areaService.findOneByAreaId(areaId),HttpStatus.OK);
     }
 }
--- src/main/java/kim/sihwan/trip_reviewer/service/AreaService.java
+++ src/main/java/kim/sihwan/trip_reviewer/service/AreaService.java
@@ -1,7 +1,9 @@
 package kim.sihwan.trip_reviewer.service;
 
 import kim.sihwan.trip_reviewer.domain.Area;
+import kim.sihwan.trip_reviewer.dto.area.AreaRequestDto;
 import kim.sihwan.trip_reviewer.dto.area.AreaResponseDto;
+import kim.sihwan.trip_reviewer.repository.AlbumRepository;
 import kim.sihwan.trip_reviewer.repository.AreaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
@@ -16,6 +18,7 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class AreaService {
     private final AreaRepository areaRepository;
+    private final AlbumRepository albumRepository;
 
     public List<AreaResponseDto> findAllByUsername(String username){
         List<Area> areas = areaRepository.findAllByMember_Username(username);
@@ -30,6 +33,32 @@ public class AreaService {
         Area area = areaRepository.findById(areaId).get();
         AreaResponseDto areaResponseDto = new AreaResponseDto(area);
         return areaResponseDto;
+    }
+
+    @Transactional
+    public void changeAreaInfo(AreaRequestDto requestDto){
+        Area area = areaRepository.findById(requestDto.getAreaId()).get();
+        if(!requestDto.getTitle().isEmpty())
+            area.changeTitle(requestDto.getTitle());
+        if(!requestDto.getColor().isEmpty())
+            area.changeColor(requestDto.getColor());
+        if(!requestDto.getAccompany().isEmpty())
+            area.changeAccompany(requestDto.getAccompany());
+        if(!requestDto.getVisitDate().isEmpty())
+            area.changeVisitDate(requestDto.getVisitDate());
+
+    }
+
+    @Transactional
+    public void initArea(Long areaId){
+        Area area = areaRepository.findById(areaId).get();
+        area.initArea();
+        List<Long> albumIdList = area.getAlbums()
+                .stream()
+                .map(album -> album.getId())
+                .collect(Collectors.toList());
+        albumRepository.deleteAllAlbumByQuery(albumIdList);
+
     }
 
 }
