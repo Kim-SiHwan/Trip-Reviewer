0117e07c0c2a6beae94aeb2747f9aa135685af6d
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 17:44:50 +0900

    security 설정

---
+++ src/main/java/kim/sihwan/trip_reviewer/config/security/SecurityConfig.java
@@ -0,0 +1,83 @@
+package kim.sihwan.trip_reviewer.config.security;
+
+import kim.sihwan.trip_reviewer.config.cors.SimpleCorsFilter;
+import kim.sihwan.trip_reviewer.config.jwt.JwtAccessDeniedHandler;
+import kim.sihwan.trip_reviewer.config.jwt.JwtAuthenticationEntryPoint;
+import kim.sihwan.trip_reviewer.config.jwt.JwtSecurityConfig;
+import kim.sihwan.trip_reviewer.config.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider tokenProvider;
+    private final SimpleCorsFilter corsFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/h2/**"
+                        ,"/favicon.ico"
+                        ,"/error"
+                );
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+
+                // enable h2-console
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2/**").permitAll()
+                .antMatchers("/api/member/save").permitAll()
+                .antMatchers("/api/member/login").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/member/new_token").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/review").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/review/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/comment/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/file/download**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+
+    }
+
+}
