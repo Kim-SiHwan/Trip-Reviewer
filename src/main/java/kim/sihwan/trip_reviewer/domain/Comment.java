853bfc18b2805cbbf4933b29f849d9f060ccfe76
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 17:49:16 +0900

    댓글 Entity

---
+++ src/main/java/kim/sihwan/trip_reviewer/domain/Comment.java
@@ -0,0 +1,53 @@
+package kim.sihwan.trip_reviewer.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+    private String content;
+    private String username;
+    private LocalDateTime createDate;
+
+
+ /*   @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;*/
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Builder
+    public Comment(String content,String username, LocalDateTime createDate) {
+        this.content = content;
+        this.username = username;
+        this.createDate = createDate;
+    }
+
+    public void changeContent(String content){
+        this.content = content;
+    }
+
+    //- 연관관계 편의 메소드 -
+//    public void addMember(Member member){
+//        this.member = member;
+//    }
+    public void addReview(Review review){
+        this.review = review;
+        this.review.getComments().add(this);
+    }
+}
