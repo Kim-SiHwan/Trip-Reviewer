8792eb106a8eea263cd0cd50aa9658108d76b682
Kim-SiHwan <tjdans4166@naver.com>
2021-02-21 17:51:37 +0900

    리뷰 앨범 응답 dto

--- src/main/java/kim/sihwan/trip_reviewer/dto/review/reviewAlbum/ReviewAlbumResponseDto.java
+++ src/main/java/kim/sihwan/trip_reviewer/dto/review/reviewAlbum/ReviewAlbumResponseDto.java
@@ -2,10 +2,12 @@ package kim.sihwan.trip_reviewer.dto.review.reviewAlbum;
 
 import kim.sihwan.trip_reviewer.domain.ReviewAlbum;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ReviewAlbumResponseDto {
     private Long id;
     private String url;
@@ -13,6 +15,7 @@ public class ReviewAlbumResponseDto {
     private String originFilename;
 
     public ReviewAlbumResponseDto(ReviewAlbum reviewAlbum){
+        System.out.println("리뷰앨범디티오!! " );
         id = reviewAlbum.getId();
         url = reviewAlbum.getUrl();
         filename = reviewAlbum.getFilename();
