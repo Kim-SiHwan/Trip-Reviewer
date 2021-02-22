94e1c3e7bf7b6a4eb92e2ff72c575df98eb495eb
Kim-SiHwan <tjdans4166@naver.com>
2021-02-23 01:17:57 +0900

    리뷰 앨범 리팩토링

--- src/main/java/kim/sihwan/trip_reviewer/service/ReviewAlbumService.java
+++ src/main/java/kim/sihwan/trip_reviewer/service/ReviewAlbumService.java
@@ -3,27 +3,18 @@ package kim.sihwan.trip_reviewer.service;
 import kim.sihwan.trip_reviewer.domain.Review;
 import kim.sihwan.trip_reviewer.domain.ReviewAlbum;
 import kim.sihwan.trip_reviewer.dto.review.ReviewRequestDto;
-import kim.sihwan.trip_reviewer.repository.ReviewAlbumRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ReviewAlbumService {
-    private final ReviewAlbumRepository reviewAlbumRepository;
-
-
-    @Transactional
-    public void deleteReviewAlbum(List<Long> reviewAlbumIdList){
-        reviewAlbumRepository.deleteAllByIdInQuery(reviewAlbumIdList);
-    }
 
     @Transactional
     public Review addReviewAlbums(ReviewRequestDto requestDto){
@@ -53,7 +44,8 @@ public class ReviewAlbumService {
     }
     public String createNewFilename(String filename){
         UUID uuid = UUID.randomUUID();
-        String newFilename= uuid.toString() +"_" + filename;
-        return newFilename;
+        return uuid.toString() +"_" + filename;
     }
+
+
 }
