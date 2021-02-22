6feed0b680994f6ae9045b6a064273d04db0efd5
Kim-SiHwan <tjdans4166@naver.com>
2021-02-22 16:52:24 +0900

    스낵바 설정

---
+++ vue-frontend/src/components/common/SnackBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-snackbar
+        v-model="snackBarInfo.open"
+        :color="snackBarInfo.color"
+        :timeout="snackBarInfo.timeout">
+      {{snackBarInfo.text}}
+    </v-snackbar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "SnackBar",
+  computed:{
+    snackBarInfo(){
+      return this.$store.state.commonStore.snackBar;
+    }
+  }
+}
+</script>
+
+<style scoped>
+body{
+}
+
+</style>
