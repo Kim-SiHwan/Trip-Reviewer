471eeb6f3ae4f44da64cd90e14ac88b5a28c2426
Kim-SiHwan <tjdans4166@naver.com>
2021-02-22 20:45:22 +0900

    import 정리, 안쓰는 메소드 삭제, entityGraph 추가

--- src/main/java/kim/sihwan/trip_reviewer/repository/ReviewTagRepository.java
+++ src/main/java/kim/sihwan/trip_reviewer/repository/ReviewTagRepository.java
@@ -2,6 +2,7 @@ package kim.sihwan.trip_reviewer.repository;
 
 import io.lettuce.core.dynamic.annotation.Param;
 import kim.sihwan.trip_reviewer.domain.ReviewTag;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,5 +16,6 @@ public interface ReviewTagRepository extends JpaRepository<ReviewTag,Long> {
     @Query("DELETE FROM ReviewTag r WHERE r.id IN :ids")
     void deleteAllByIdInQuery(@Param("ids") List<Long> ids);
 
+    @EntityGraph(attributePaths = {"review","tag","review.reviewAlbums","review.member","review.comments"} , type = EntityGraph.EntityGraphType.LOAD)
     List<ReviewTag> findAllByTag_Id(Long id);
 }
