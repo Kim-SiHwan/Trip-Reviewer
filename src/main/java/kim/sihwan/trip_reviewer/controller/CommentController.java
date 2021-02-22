84d8e1c6ee7fef969afe32a4ee59c16dccf62033
Kim-SiHwan <tjdans4166@naver.com>
2021-02-23 01:17:30 +0900

    댓글 리팩토링

--- src/main/java/kim/sihwan/trip_reviewer/controller/CommentController.java
+++ src/main/java/kim/sihwan/trip_reviewer/controller/CommentController.java
@@ -5,7 +5,6 @@ import kim.sihwan.trip_reviewer.dto.comment.CommentResponseDto;
 import kim.sihwan.trip_reviewer.dto.comment.CommentUpdateRequestDto;
 import kim.sihwan.trip_reviewer.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,29 +17,24 @@ import java.util.List;
 public class CommentController {
     private final CommentService commentService;
 
-    public List<CommentResponseDto> getComments(Long reviewId){
+    private List<CommentResponseDto> getComments(Long reviewId){
         return commentService.findAllComments(reviewId);
     }
 
-
     @GetMapping("/{reviewId}")
-    public ResponseEntity findAllCommentsByReviewId(@PathVariable("reviewId")Long reviewId){
-        List<CommentResponseDto> result = getComments(reviewId);
-        return new ResponseEntity(result,HttpStatus.OK);
+    public ResponseEntity<CommentResponseDto> findAllCommentsByReviewId(@PathVariable("reviewId")Long reviewId){
+//        List<CommentResponseDto> result = getComments(reviewId);
+        return new ResponseEntity(commentService.findAllComments(reviewId),HttpStatus.OK);
     }
-
 
     @PostMapping
-    public ResponseEntity addComment(@RequestBody CommentRequestDto requestDto){
+    public void addComment(@RequestBody CommentRequestDto requestDto){
         commentService.addComment(requestDto);
-        return new ResponseEntity(getComments(requestDto.getReviewId()),HttpStatus.OK);
     }
 
-    @DeleteMapping("/{reviewId}/{commentId}")
-    public ResponseEntity deleteComment(@PathVariable("reviewId")Long reviewId,
-                                        @PathVariable("commentId")Long commentId){
+    @DeleteMapping("/{commentId}")
+    public void deleteComment(@PathVariable("commentId")Long commentId){
         commentService.deleteComment(commentId);
-        return new ResponseEntity(getComments(reviewId),HttpStatus.OK);
     }
 
     @PutMapping
@@ -50,9 +44,9 @@ public class CommentController {
     }
 
     @GetMapping("/my/{username}")
-    public ResponseEntity getAllCommentsByUsername(@PathVariable("username")String username){
-        List<CommentResponseDto> result = commentService.findAllCommentsByUsername(username);
-        return new ResponseEntity(result,HttpStatus.OK);
+    public ResponseEntity<CommentResponseDto> getAllCommentsByUsername(@PathVariable("username")String username){
+//        List<CommentResponseDto> result = commentService.findAllCommentsByUsername(username);
+        return new ResponseEntity(commentService.findAllCommentsByUsername(username),HttpStatus.OK);
     }
 
 }
--- src/main/java/kim/sihwan/trip_reviewer/service/CommentService.java
+++ src/main/java/kim/sihwan/trip_reviewer/service/CommentService.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,63 +18,52 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CommentService {
+
     private final CommentRepository commentRepository;
     private final ReviewRepository reviewRepository;
 
-    public CommentResponseDto findOneByCommentId(Long commentId){
-        Comment comment = commentRepository.findById(commentId).get();
-        CommentResponseDto result = new CommentResponseDto(comment);
-        return result;
-    }
 
-    public List<CommentResponseDto> findAllCommentsByUsername(String username){
-        List<Comment> comments = commentRepository.findAllByUsername(username);
-        System.out.println("코멘트 서비스 : "+username);
-        List<CommentResponseDto> list = new ArrayList<>();
-        list = comments
+
+
+//     * @Slf4j << log4j,logback,log4j2
+
+    public List<CommentResponseDto> findAllCommentsByUsername(String username) {
+        return commentRepository.findAllByUsername(username)
                 .stream()
-                .map(comment -> new CommentResponseDto(comment))
+                .map(CommentResponseDto::new)
                 .collect(Collectors.toList());
-        list.forEach(l-> System.out.println("리스트 :" +l.getUsername()+" "+l.getContent()));
-        return list;
 
     }
 
-    public List<CommentResponseDto> findAllComments(Long reviewId){
-        List<Comment> comments = commentRepository.findAllByReview_Id(reviewId);
-        List<CommentResponseDto> list = comments
+    public List<CommentResponseDto> findAllComments(Long reviewId) {
+        return commentRepository.findAllByReview_Id(reviewId)
                 .stream()
-                .map(comment -> new CommentResponseDto(comment))
+                .map(CommentResponseDto::new)
                 .collect(Collectors.toList());
-        return list;
     }
 
     @Transactional
-    public void addComment(CommentRequestDto requestDto){
+    public void addComment(CommentRequestDto requestDto) {
         Long reviewId = requestDto.getReviewId();
         Review review = reviewRepository.findById(reviewId).get();
-        System.out.println("애드코멘트 : "+review.getReviewAlbums().size());
         Comment comment = requestDto.toEntity(requestDto);
         comment.addReview(review);
+        /**
+         * review.addComment(review)
+         */
     }
 
     @Transactional
-    public void updateComment(CommentUpdateRequestDto updateRequestDto){
+    public void updateComment(CommentUpdateRequestDto updateRequestDto) {
         Comment comment = commentRepository.findById(updateRequestDto.getId()).get();
         comment.changeContent(updateRequestDto.getContent());
 
     }
 
     @Transactional
-    public void deleteComment(Long commentId){
+    public void deleteComment(Long commentId) {
         Comment comment = commentRepository.findById(commentId).get();
         commentRepository.delete(comment);
     }
-
-    @Transactional
-    public void deleteAllWithReview(List<Long> commentIdList){
-        commentRepository.deleteAllByIdInQuery(commentIdList);
-    }
-
 
 }
