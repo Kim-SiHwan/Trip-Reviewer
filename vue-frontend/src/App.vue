ca1bf6793ede19f673433cca1c8b400296269fe1
Kim-SiHwan <tjdans4166@naver.com>
2021-02-23 22:19:33 +0900

    App.vue

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,16 +1,80 @@
 <template>
   <v-app id="app">
 
+    <v-app-bar
+        absolute
+        color="orange lighten-2"
+        elevation="10"
+        dark
+    >
+
+      <v-toolbar-title>
+        <router-link :to="{path:'/'}"
+                     style="color: white; text-decoration: none">
+          Trip-Reviewer <small>당신만의 여행지도</small>
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+
+      <v-btn
+          color="orange lighten-1"
+          class="ml-4"
+          small
+          v-if="$store.getters.isAuthenticated">
+        <router-link to="/main"
+                     style="color: white; text-decoration: none">지도보기</router-link>
+      </v-btn>
+      <v-btn
+          color="orange lighten-1"
+          class="ml-4"
+          small
+          v-if="!$store.getters.isAuthenticated">
+        <router-link to="/login"
+                     style="color: white; text-decoration: none">로그인</router-link>
+      </v-btn>
+
+      <v-btn
+          color="orange lighten-1"
+          class="ml-4 pt-4"
+          small
+          v-else @click="logout" >
+        <p style="color: white; text-decoration: none">로그아웃</p>
+      </v-btn>
+      <router-link to="/join"
+                   style="color: white; text-decoration: none">
+        <v-btn
+          color="orange lighten-1"
+          small
+          class="ml-4">회원가입</v-btn>
+
+      </router-link>
+      <router-link to="/reviewList"
+                   style="color: white; text-decoration: none">
+      <v-btn
+          color="orange lighten-1"
+          small
+          class="ml-4">리뷰</v-btn>
+
+      </router-link>
+
+      <router-link to="/my"
+                   style="color: white; text-decoration: none">
+        <v-btn
+            v-if="$store.getters.isAuthenticated"
+            color="orange lighten-1"
+            small
+            class="ml-4">마이페이지</v-btn>
+
+      </router-link>
+    </v-app-bar>
 
 
 
 
-    <router-link to="/">메인</router-link>
-    <router-link to="/login">로그인</router-link>
-    <router-link to="/join">회원가입</router-link>
-    <router-link @click="alert('as')" to="/reviewList">리뷰</router-link>
     <hr>
-    <router-view></router-view>
+    <router-view style="margin-top: 70px"></router-view>
 
     <v-snackbar
         v-model="snackBarInfo.open"
@@ -20,7 +84,6 @@
     </v-snackbar>
 
 
-    <h1>App.vue부분</h1>
   </v-app>
 
 
@@ -34,9 +97,8 @@ export default {
   components:{
   },
   methods:{
-    hh(){
-      this.$store.dispatch('R',5);
-
+    logout(){
+      this.$store.dispatch('REQUEST_LOGOUT',this.$store.state.memberStore.username);
     }
   },
   computed:{
