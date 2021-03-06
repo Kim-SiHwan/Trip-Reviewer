f362f969e32bbf67cd6bc45fc698f3061226645c
Kim-SiHwan <tjdans4166@naver.com>
2021-03-07 02:30:08 +0900

    application runner 변경 h2 -> mariadb

--- src/main/java/kim/sihwan/trip_reviewer/config/AppConfig.java
+++ src/main/java/kim/sihwan/trip_reviewer/config/AppConfig.java
@@ -1,96 +1,22 @@
 package kim.sihwan.trip_reviewer.config;
 
 
-import kim.sihwan.trip_reviewer.domain.Area;
-import kim.sihwan.trip_reviewer.domain.Member;
-import kim.sihwan.trip_reviewer.repository.AreaRepository;
-import kim.sihwan.trip_reviewer.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class AppConfig implements ApplicationRunner {
-    private final MemberRepository memberRepository;
-    private final AreaRepository areaRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        Member member = Member
-                .builder()
-                .username("admin")
-                .password(passwordEncoder.encode("admin"))
-                .nickname("HELLO ADMIN")
-                .role("ROLE_ADMIN")
-                .build();
-        memberRepository.save(member);
-        init(member);
-
-        Member member2 = Member
-                .builder()
-                .username("user")
-                .password(passwordEncoder.encode("user"))
-                .nickname("HELLO USER")
-                .role("ROLE_USER")
-                .build();
-        memberRepository.save(member2);
-        init(member2);
-        Member member3 = Member
-                .builder()
-                .username("test")
-                .password("test")
-                .nickname("HELLO TEST")
-                .role("ROLE_USER")
-                .build();
-        memberRepository.save(member3);
-        init(member3);
+        log.info("Start App!");
     }
 
-    public void init(Member member) {
-        try {
-            JSONParser jsonParser = new JSONParser();
-            String path = AppConfig.class.getResource("").getPath();
-
-            InputStream in5 = this.getClass().getResourceAsStream("/custom.json");
-
-            Object obj = jsonParser.parse(new InputStreamReader(in5));
-
-
-            JSONObject jsonObj = (JSONObject) jsonParser.parse(obj.toString());
-            JSONArray array = (JSONArray) jsonObj.get("features");
-            for (int i = 0; i < array.size(); i++) {
-                JSONObject jj = (JSONObject) array.get(i);
-                JSONObject jj2 = (JSONObject) jj.get("properties");
-
-                Area area = Area.builder()
-                        .name(jj2.get("SIG_KOR_NM").toString())
-                        .idx(Integer.parseInt(jj2.get("idx").toString()))
-                        .accompany("")
-                        .title("")
-                        .color("#ffffff")
-                        .visitDate("")
-                        .build();
-                area.addMember(member);
-                areaRepository.save(area);
-
-            }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
 
 }
