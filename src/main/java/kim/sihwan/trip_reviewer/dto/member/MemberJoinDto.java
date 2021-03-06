d84489ee56c728e6b531add721f1c00bba715802
Kim-SiHwan <tjdans4166@naver.com>
2021-03-06 17:59:59 +0900

    Validation 정규식 작성

--- src/main/java/kim/sihwan/trip_reviewer/dto/member/MemberJoinDto.java
+++ src/main/java/kim/sihwan/trip_reviewer/dto/member/MemberJoinDto.java
@@ -5,15 +5,20 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
 
 @Getter
 @Builder
 public class MemberJoinDto {
+    @NotBlank
     @NotEmpty(message = "아이디는 필수 항목입니다.")
+    @Pattern(regexp = "^(?=\\S+$)(?=.*?[a-z])(?=.*?[0-9]).{6,13}$", message ="아이디는 공백 없이 특수문자를 제외한 영문자와 숫자 조합으로  6~12글자로 작성해주세요." )
     private String username;
 
+    @NotNull
     @NotEmpty(message = "비밀번호는 필수 항목입니다.")
+    @Pattern(regexp = "^(?=\\S+$)(?=.*?[a-z])(?=.*?[0-9]).{6,13}$" , message ="비밀번호는 공백 없이 영문자와 숫자 조합으로 6~12글자로 작성해주세요.."
+    )
     private String password;
 
     public Member toEntity(MemberJoinDto joinDto, PasswordEncoder passwordEncoder){
