e3b91900f641ac2fa5e8dea817ec5ad1f5ee95e3
Kim-SiHwan <tjdans4166@naver.com>
2021-03-07 15:17:16 +0900

    multipartfile 사이즈 제한

--- src/main/java/kim/sihwan/trip_reviewer/exception/ErrorCode.java
+++ src/main/java/kim/sihwan/trip_reviewer/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     INVALID_TOKEN(4,"잘못된 토큰정보입니다. 다시 로그인을 진행해주세요."),
     EXPIRED_TOKEN(5,"만료된 토큰입니다. 다시 로그인을 진행해주세요."),
     NON_LOGIN(6,"로그인이 필요합니다."),
-    FILE_SIZE_OVER(7,"사진당 1MB 미만의 사진만 가능합니다."),
+    FILE_SIZE_OVER(7,"사진당 5MB 미만의 사진만 가능합니다."),
     DELETED_REVIEW(8,"삭제된 게시글입니다."),
     FORBIDDEN_ACCESS(9,"접근할 수 없습니다"),
     DIFFERENT_USER_ACCESS(10,"작성자만 수정, 삭제 할 수 있습니다.");
